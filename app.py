46b3dc8fc453568c1a33a16423fce4d391cf313f
Electroduty <gc997716@gmail.com>
2021-06-30 21:06:29 +0530

    commit

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ con = sqlite3.connect('Videofilelink.db')
 cur = con.cursor()
 
 #home route
-@app.route('/home')
+@app.route('/')
 def home():
     # return render_template('home.html')
     return render_template('real_home.html')
